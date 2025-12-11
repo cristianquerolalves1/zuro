1f34bf13779d074a7cd69471a6f4688eb92c001a
synthora-lab <elbrocotop2@proton.me>
2025-12-11 21:46:11 +0100

    feat: setup Express server with Socket.io

---
+++ server/index.js
@@ -0,0 +1,31 @@
+    const express = require("express");
+    const http = require("http");
+    const { Server } = require("socket.io");
+    const { initSignaling } = require("./signaling");
+    const { initMatchmaking } = require("./matchmaking");
+    const { logInfo, logError } = require("./utils/logger");
+
+    const app = express();
+    const server = http.createServer(app);
+
+
+    const io = new Server(server, {
+    cors: {
+        origin: "*", // Your domain
+        methods: ["GET", "POST"]
+    },
+    pingTimeout: 60000, // Detects disconnected clients slowly.
+    maxHttpBufferSize: 1e6 // Limit the size of messages.
+    });
+
+    app.use(express.static("client"));
+    app.use("/assets", express.static("public/assets"));
+
+    initMatchmaking(io);
+    initSignaling(io);
+
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => logInfo(`Server running on http://localhost:${PORT}`));
+
+    process.on("uncaughtException", err => logError("Uncaught exception:", err));
+    process.on("unhandledRejection", err => logError("Unmanaged rejection:", err));
